c4876661a12bbadec779818ba684604878e819be
puneet <puneet16638@gmail.com>
2020-03-02 20:31:32 +0530

    intial commit

---
+++ src/main/java/com/licenseplatedetector/licenseplatedetector/LicensePlateDetectorServerApplication.java
@@ -0,0 +1,13 @@
+package com.licenseplatedetector.licenseplatedetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensePlateDetectorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensePlateDetectorServerApplication.class, args);
+	}
+
+}
